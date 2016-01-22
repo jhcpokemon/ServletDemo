26243a3e23f784459c796f438b4b285893865a94
jhcpokemon <jhc851267@gmail.com>
2016-01-22 22:08:55 +0800

    Add send email demo

---
+++ src/servlet/SendEmail.java
@@ -0,0 +1,58 @@
+package servlet;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+@WebServlet(name = "SendEmail")
+public class SendEmail extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String to = "abcd@163.com";
+        String from = "local@163.com";
+        String host = "localhost";
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties);
+
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setFrom(new InternetAddress(from));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.setSubject("Hello");
+            mimeMessage.setText("Hello World");
+            Transport.send(mimeMessage);
+            String title = "Send Email";
+            String res = "Sent message successfully....";
+            String docType =
+                    "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                            "transitional//en\">\n";
+            out.println(docType +
+                    "<html>\n" +
+                    "<head><title>" + title + "</title></head>\n" +
+                    "<body bgcolor=\"#f0f0f0\">\n" +
+                    "<h1 align=\"center\">" + title + "</h1>\n" +
+                    "<p align=\"center\">" + res + "</p>\n" +
+                    "</body></html>");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
